c5df5851be8a623f2a51f6a88f8ea141ab6e8ae8
YeWang <yw1576@nyu.edu>
2025-07-31 20:05:00 -0400

    package

--- CTTB/R/CTTB.R
+++ CTTB/R/CTTB.R
@@ -1,12 +1,12 @@
 CTTB <- function(data, seed = NULL, Y, D, S, X = NULL, W = NULL, Pscore = NULL,
-                 regression.splitting = FALSE, cv_fold = 5, trim_l = 0, trim_u = 1,
-                 aggBounds = TRUE, IM_cv = TRUE, alpha = 0.05, cBounds = FALSE, X_moderator = NULL, 
+                 splitting = FALSE, cv_fold = 5, trim_l = 0, trim_u = 1,
+                 aggBounds = TRUE, IM_cv = TRUE, alpha = 0.05, cBounds = FALSE, X_moderator = NULL,
                  LeeBounds = FALSE, direction = NULL, cond.mono = TRUE, message = TRUE){
-  
+
   if (is.null(data)){
     stop("No dataset specified")
   }
-  
+
   if (!is.null(seed)){
     set.seed(seed)
   }
@@ -16,17 +16,17 @@ CTTB <- function(data, seed = NULL, Y, D, S, X = NULL, W = NULL, Pscore = NULL,
   }else if (!all(data[, D] %in% 0:1)){
     stop("Non-binary treatment")
   }
-  
+
   if (is.null(Pscore) & message == TRUE){
     cat("Propensity scores are not specified and will be estimated using X.", "\n")
   }
-  
+
   if (is.null(W)){
     W <- "uni_w"
     data[ W] <- 1
   }
   N <- sum(data[, W])
-  
+
   if (is.null(S)){
     stop("No missing/selection indicator specified")
   }else if (!all(data[, S] %in% 0:1)){
@@ -38,7 +38,7 @@ CTTB <- function(data, seed = NULL, Y, D, S, X = NULL, W = NULL, Pscore = NULL,
       cat("The average missing rate is ", round(S0_sum/N, 4), "\n")
     }
   }
-  
+
   bo <- 0
   if (is.null(Y)){
     stop("No outcome specified")
@@ -46,8 +46,8 @@ CTTB <- function(data, seed = NULL, Y, D, S, X = NULL, W = NULL, Pscore = NULL,
     cat("Binary outcome", "\n")
     bo <- 1
   }
-  
-  
+
+
   if (is.null(X)){
     cat("No covariate. Lee bounds will be used", "\n")
     aggBounds <- FALSE
@@ -61,44 +61,44 @@ CTTB <- function(data, seed = NULL, Y, D, S, X = NULL, W = NULL, Pscore = NULL,
       }
     }
   }
-  
+
   data0 <- data[data[, D] == 0, ]
   data1 <- data[data[, D] == 1, ]
-  
+
   folds_0 <- cut(seq(1, nrow(data0)), breaks = cv_fold, labels = FALSE)
   folds_1 <- cut(seq(1, nrow(data1)), breaks = cv_fold, labels = FALSE)
-  
-  
-  # check splitting and ensure there are more than 
+
+
+  # check splitting and ensure there are more than
   # 5 treated and untreated obs in each fold
   flag <- TRUE
   while (flag){
     split_index_0 <- sample(folds_0, length(folds_0), replace = F)
     split_index_1 <- sample(folds_1, length(folds_1), replace = F)
-    
+
     data0$split_index <- split_index_0
     data1$split_index <- split_index_1
-    
+
     dat_final <- data.frame(rbind(data0, data1))
     flags <- rep(NA, cv_fold)
     for (cc in 1:cv_fold){
       d_train <- dat_final[dat_final$split_index != cc, ]
-      flags[cc] <- sum(d_train[d_train[, D] == 1, S]) < 5 | 
-        sum(1 - d_train[d_train[, D] == 1, S]) < 5 | 
-        sum(d_train[d_train[, D] == 0, S]) < 5 | 
+      flags[cc] <- sum(d_train[d_train[, D] == 1, S]) < 5 |
+        sum(1 - d_train[d_train[, D] == 1, S]) < 5 |
+        sum(d_train[d_train[, D] == 0, S]) < 5 |
         sum(1 - d_train[d_train[, D] == 0, S]) < 5
     }
     flag <- mean(flags) > 0
   }
-  
+
   result <- list()
   if (bo == 1){
     if (LeeBounds){
       lee.result <- LBounds_binary(dat_final, Y, X, S, D, W, Pscore,
                                    cond.mono, direction)
       if (IM_cv){
-        IM_cv_lee <- IM_cv(lee.result[["tau_l_est_lee"]], lee.result[["tau_u_est_lee"]], 
-                           lee.result[["se_tau_l_est_lee"]], lee.result[["se_tau_u_est_lee"]], 
+        IM_cv_lee <- IM_cv(lee.result[["tau_l_est_lee"]], lee.result[["tau_u_est_lee"]],
+                           lee.result[["se_tau_l_est_lee"]], lee.result[["se_tau_u_est_lee"]],
                            alpha, sum(dat[, S]))
       }
       TB_dat <- data.frame("Estimate" = c(lee.result[["tau_l_est_lee"]], lee.result[["tau_u_est_lee"]]),
@@ -111,17 +111,17 @@ CTTB <- function(data, seed = NULL, Y, D, S, X = NULL, W = NULL, Pscore = NULL,
       }
       TB_dat$Method <- rep(c("TB"), 2)
       TB_dat$Type <- c("Lower", "Upper")
-      
+
       result[["TB"]] <- TB_dat
     }
-    
+
     if (aggBounds){
       agg.result <- aggBounds_binary(dat_final = dat_final, seed = seed, Y, X, S, D, W, Pscore,
-                                     regression.splitting = regression.splitting,
+                                     splitting = splitting,
                                      cv_fold = cv_fold, trim_l, trim_u)
       if (IM_cv){
-        IM_cv_agg <- IM_cv(agg.result[["tau_l_est_avg"]], agg.result[["tau_u_est_avg"]], 
-                           agg.result[["se_tau_l_est_avg"]], agg.result[["se_tau_u_est_avg"]], 
+        IM_cv_agg <- IM_cv(agg.result[["tau_l_est_avg"]], agg.result[["tau_u_est_avg"]],
+                           agg.result[["se_tau_l_est_avg"]], agg.result[["se_tau_u_est_avg"]],
                            alpha, sum(dat[, S]))
       }
       CTTB_dat <- data.frame("Estimate" = c(agg.result[["tau_l_est_avg"]], agg.result[["tau_u_est_avg"]]),
@@ -134,7 +134,7 @@ CTTB <- function(data, seed = NULL, Y, D, S, X = NULL, W = NULL, Pscore = NULL,
       }
       CTTB_dat$Method <- rep(c("CTTB"), 2)
       CTTB_dat$Type <- c("Lower", "Upper")
-      
+
       result[["CTTB"]] <- CTTB_dat
     }
   }else{
@@ -142,8 +142,8 @@ CTTB <- function(data, seed = NULL, Y, D, S, X = NULL, W = NULL, Pscore = NULL,
       lee.result <- LBounds_cont(dat_final, Y, X, S, D, W, Pscore,
                                  cond.mono, direction, message=message)
       if (IM_cv){
-        IM_cv_lee <- IM_cv(lee.result[["tau_l_est_lee"]], lee.result[["tau_u_est_lee"]], 
-                           lee.result[["se_tau_l_est_lee"]], lee.result[["se_tau_u_est_lee"]], 
+        IM_cv_lee <- IM_cv(lee.result[["tau_l_est_lee"]], lee.result[["tau_u_est_lee"]],
+                           lee.result[["se_tau_l_est_lee"]], lee.result[["se_tau_u_est_lee"]],
                            alpha, sum(dat[, S]))
       }
       TB_dat <- data.frame("Estimate" = c(lee.result[["tau_l_est_lee"]], lee.result[["tau_u_est_lee"]]),
@@ -152,21 +152,21 @@ CTTB <- function(data, seed = NULL, Y, D, S, X = NULL, W = NULL, Pscore = NULL,
       TB_dat$CI95_upper <- TB_dat$Estimate + qnorm(1 - alpha/2) * TB_dat$SE
       if (IM_cv){
         TB_dat$CI95_lower_IM <- TB_dat$Estimate - IM_cv_lee * TB_dat$SE
-        TB_dat$CI95_upper_IM <- TB_dat$Estimate + IM_cv_lee * TB_dat$SE 
+        TB_dat$CI95_upper_IM <- TB_dat$Estimate + IM_cv_lee * TB_dat$SE
       }
       TB_dat$Method <- rep(c("TB"), 2)
       TB_dat$Type <- c("Lower", "Upper")
-      
+
       result[["TB"]] <- TB_dat
     }
-    
+
     if (aggBounds){
       agg.result <- aggBounds_cont(dat_final, seed = seed, Y, X, S, D, W, Pscore,
-                                   regression.splitting = regression.splitting,
+                                   splitting = splitting,
                                    cv_fold = cv_fold, trim_l, trim_u)
       if (IM_cv){
-        IM_cv_agg <- IM_cv(agg.result[["tau_l_est_avg"]], agg.result[["tau_u_est_avg"]], 
-                           agg.result[["se_tau_l_est_avg"]], agg.result[["se_tau_u_est_avg"]], 
+        IM_cv_agg <- IM_cv(agg.result[["tau_l_est_avg"]], agg.result[["tau_u_est_avg"]],
+                           agg.result[["se_tau_l_est_avg"]], agg.result[["se_tau_u_est_avg"]],
                            alpha, sum(dat[, S]))
       }
       CTTB_dat <- data.frame("Estimate" = c(agg.result[["tau_l_est_avg"]], agg.result[["tau_u_est_avg"]]),
@@ -174,28 +174,28 @@ CTTB <- function(data, seed = NULL, Y, D, S, X = NULL, W = NULL, Pscore = NULL,
       CTTB_dat$CI95_lower <- CTTB_dat$Estimate - qnorm(1 - alpha/2) * CTTB_dat$SE
       CTTB_dat$CI95_upper <- CTTB_dat$Estimate + qnorm(1 - alpha/2) * CTTB_dat$SE
       if (IM_cv){
-        CTTB_dat$CI95_lower_IM <- CTTB_dat$Estimate - IM_cv_lee * CTTB_dat$SE
-        CTTB_dat$CI95_upper_IM <- CTTB_dat$Estimate + IM_cv_lee * CTTB_dat$SE 
+        CTTB_dat$CI95_lower_IM <- CTTB_dat$Estimate - IM_cv_agg * CTTB_dat$SE
+        CTTB_dat$CI95_upper_IM <- CTTB_dat$Estimate + IM_cv_agg * CTTB_dat$SE
       }
       CTTB_dat$Method <- rep(c("CTTB"), 2)
       CTTB_dat$Type <- c("Lower", "Upper")
-      
+
       result[["CTTB"]] <- CTTB_dat
       result[["Ps_k"]] <- agg.result[["Ps_k"]]
     }
-    
+
     if (cBounds){
       if (is.null(X_moderator)){
         stop("No moderator is provided.")
       }
       cond.result <- condBounds_cont(dat_final, seed = seed, Y, X, S, D, W, Pscore,
-                                     X_moderator, regression.splitting = regression.splitting,
+                                     X_moderator, splitting = splitting,
                                      cv_fold = cv_fold)
       # if (IM_cv){
       #   IM_cv_cond <- rep(NA, nrow(X_moderator))
       #   for (rr in 1:nrow(X_moderator)){
-      #     IM_cv_cond[rr] <- IM_cv(cond.result[["tau_l_est_avg"]][rr], cond.result[["tau_u_est_avg"]][rr], 
-      #                             cond.result[["se_tau_l_est_avg"]][rr], cond.result[["se_tau_u_est_avg"]][rr], 
+      #     IM_cv_cond[rr] <- IM_cv(cond.result[["tau_l_est_avg"]][rr], cond.result[["tau_u_est_avg"]][rr],
+      #                             cond.result[["se_tau_l_est_avg"]][rr], cond.result[["se_tau_u_est_avg"]][rr],
       #                             alpha, sum(dat[, S]))
       #   }
       # }
@@ -211,18 +211,18 @@ CTTB <- function(data, seed = NULL, Y, D, S, X = NULL, W = NULL, Pscore = NULL,
       cond_dat$Type <- rep(c("Lower", "Upper"), each = nrow(X_moderator))
       colnames(X_moderator) <- c("Const", paste0("X", seq(1:(ncol(X_moderator) - 1))))
       cond_dat <- cbind(cond_dat, X_moderator)
-      
-      result[["cBounds"]] <- cond_dat
+
+      result[["cB"]] <- cond_dat
     }
   }
-  result[["parameters"]] <- c("bo" = bo, "LeeBounds" = LeeBounds, "aggBounds" = aggBounds, 
+  result[["parameters"]] <- c("bo" = bo, "LeeBounds" = LeeBounds, "aggBounds" = aggBounds,
                               "cBounds" = cBounds, "IM_cv" = IM_cv, "cond.mono" = cond.mono)
   class(result) <- "CTTB"
   result$call <- match.call()
   return(result)
 }
- 
- 
+
+
 IM_cv <- function(tau_l, tau_u, se_tau_l, se_tau_u, alpha, N){
   cv.fun <- function(cv, tau_l, tau_u, se_tau_l, se_tau_u, alpha, N){
     pnorm(cv + (tau_u - tau_l) / max(se_tau_l, se_tau_u)) - pnorm(-cv) - (1 - alpha)
@@ -242,7 +242,7 @@ print.CTTB <- function(result, ...) {
   }
   if (result[["parameters"]]["cBounds"]){
     cat("Estimates of conditional bounds:", "\n")
-    print(result[["cBounds"]])
+    print(result[["cB"]])
   }
   invisible(result)
 }
@@ -258,7 +258,7 @@ summary.CTTB <- function(result, ...) {
   }
   if (result[["parameters"]]["cBounds"]){
     cat("Estimates of conditional bounds:", "\n")
-    print(result[["cBounds"]])
+    print(result[["cB"]])
   }
   invisible(result)
 }
@@ -272,6 +272,8 @@ plot.CTTB <- function(result, type = "aggregated", moderator_plot = 1, ...) {
       }
     }else if (result[["parameters"]]["aggBounds"]){
       p.data <- result[["CTTB"]]
+    }else{
+      stop("Aggregated bounds are not estimated")
     }
     pd <- position_dodge(width=0.2)
     coefs_p <- ggplot(p.data, aes(Method, Estimate, color=Type)) +
@@ -279,13 +281,13 @@ plot.CTTB <- function(result, type = "aggregated", moderator_plot = 1, ...) {
       scale_color_manual(name="Estimand",values=c("grey", "black")) +
       scale_shape_manual(name="Method",values=c(19, 19)) +
       theme_bw() +
-      geom_errorbar(aes(Method, ymin=CI95_lower, ymax=CI95_upper), width=0.2, position=pd, linetype = "dashed") + 
+      geom_errorbar(aes(Method, ymin=CI95_lower, ymax=CI95_upper), width=0.2, position=pd, linetype = "dashed") +
       xlab('') + ylab('') +
-      theme(plot.title = element_text(hjust = 0.5), text = element_text(size=25), axis.text.x = element_text(angle = 0), legend.position = "top") + 
+      theme(plot.title = element_text(hjust = 0.5), text = element_text(size=25), axis.text.x = element_text(angle = 0), legend.position = "top") +
       ylab("Estimates") + coord_flip()
-    
+
     y_min <- min(p.data$CI95_upper, na.rm = TRUE)
-    
+
     if (result[["parameters"]]["IM_cv"]){
       coefs_p <- coefs_p + geom_errorbar(aes(Method, ymin=CI95_lower_IM, ymax=CI95_upper_IM), width=0.2, position=pd) +
         annotate("segment", x = 2.2, xend = 2.2, y = y_min - 0.6, yend = y_min - 0.2, linetype = "dashed", size = 0.5) +
@@ -293,14 +295,14 @@ plot.CTTB <- function(result, type = "aggregated", moderator_plot = 1, ...) {
         annotate("segment", x = 2.5, xend = 2.5, y = y_min - 0.6, yend = y_min - 0.2, linetype = "solid", size = 0.5) +
         annotate("text", x = 2.5, y = y_min, label = "IM CI for the Estimate", hjust = 0, size = 5)
     }else{
-      coefs_p <- coefs_p + 
+      coefs_p <- coefs_p +
         annotate("segment", x = 2.2, xend = 2.2, y = y_min - 0.6, yend = y_min - 0.2, linetype = "dashed", size = 0.5) +
         annotate("text", x = 2.2, y = y_min, label = "CI for the Bounds", hjust = 0, size = 5)
     }
     return(coefs_p)
   }else if (type == "conditional"){
     if (result[["parameters"]]["cBounds"]){
-      p.data.m <- result[["cBounds"]]
+      p.data.m <- result[["cB"]]
       p.data.m$Type <- factor(p.data.m$Type, levels = c("Upper", "Lower"),
                               labels = c("Upper Bound", "Lower Bound"))
       moderator_name <- sym(paste0("X", moderator_plot))
--- CTTB/R/CTTB_functions_binary.R
+++ CTTB/R/CTTB_functions_binary.R
@@ -1,25 +1,25 @@
 aggBounds_binary_oneshot <- function(dat_bo, seed = NULL, Y, X, S, D, W, Pscore,
-                                     regression.splitting = F, cv_fold = cv_fold, trim_l, trim_u){
-  
+                                     splitting = F, cv_fold = cv_fold, trim_l, trim_u){
+
   scores_l_k <- scores_u_k <- qar_k <- c()
   score1s_l_k <- score1s_u_k <- score0s_k <- score0s_l_k <- score0s_u_k <- score1s_k <- c()
   directions_k <- cor1s_k <- cor0s_k <- qar_help_k <- qar_hurt_k <- c()
-  
+
   for (k in 1:cv_fold){
-    
+
     dat.s1 <- dat_bo[dat_bo$split_index != k, ]
     dat.s2 <- dat_bo[dat_bo$split_index == k, ]
-    
+
     X_eval_k <- dat.s2[, X]
     Y_eval_k <- dat.s2[, Y]
     D_eval_k <- dat.s2[, D]
     S_eval_k <- dat.s2[, S]
     W_eval_k <- dat.s2[, W]
     Ps_eval_k <- dat.s2[, Pscore]
-    
+
     dat.s1.0 <- dat.s1[dat.s1[, D] == 0, ]
     dat.s1.1 <- dat.s1[dat.s1[, D] == 1, ]
-    
+
     if (is.null(seed)){
       pf0 <- probability_forest(as.matrix(dat.s1.0[, X]), as.factor(dat.s1.0[, S]), sample.weights = dat.s1.0[, W])
       pf1 <- probability_forest(as.matrix(dat.s1.1[, X]), as.factor(dat.s1.1[, S]), sample.weights = dat.s1.1[, W])
@@ -27,28 +27,28 @@ aggBounds_binary_oneshot <- function(dat_bo, seed = NULL, Y, X, S, D, W, Pscore,
       pf0 <- probability_forest(as.matrix(dat.s1.0[, X]), as.factor(dat.s1.0[, S]), sample.weights = dat.s1.0[, W], seed = seed)
       pf1 <- probability_forest(as.matrix(dat.s1.1[, X]), as.factor(dat.s1.1[, S]), sample.weights = dat.s1.1[, W], seed = seed)
     }
-    
+
     pf0_pred <- predict(pf0, newdata = (as.matrix(X_eval_k)))
     pf1_pred <- predict(pf1, newdata = (as.matrix(X_eval_k)))
-    
+
     one_q0 <- pf0_pred$predictions[, 2]
     one_q1 <- pf1_pred$predictions[, 2]
-    
+
     directions <- as.numeric(one_q0 < one_q1)
-    
+
     one_q_l <- directions * (one_q0 / one_q1) + (1 - directions) * (one_q1 / one_q0)
-    
+
     one_q_l[one_q_l < trim_l] <- trim_l
     one_q_l[one_q_l > trim_u] <- trim_u
-    
+
     one_q_u <- 1 - one_q_l
-    
+
     one_q_l_order <- order(one_q_l)
     one_q_u_order <- order(one_q_u)
-    
+
     one_q_l_ordered <- one_q_l[one_q_l_order]
     one_q_u_ordered <- one_q_u[one_q_u_order]
-    
+
     if (is.null(seed)){
       xi_model_help <- probability_forest(as.matrix(dat.s1.1[dat.s1.1[, S] == 1, X]), as.factor(dat.s1.1[dat.s1.1[, S] == 1, Y]))
       xi_model_hurt <- probability_forest(as.matrix(dat.s1.0[dat.s1.0[, S] == 1, X]), as.factor(dat.s1.0[dat.s1.0[, S] == 1, Y]))
@@ -56,36 +56,36 @@ aggBounds_binary_oneshot <- function(dat_bo, seed = NULL, Y, X, S, D, W, Pscore,
       xi_model_help <- probability_forest(as.matrix(dat.s1.1[dat.s1.1[, S] == 1, X]), as.factor(dat.s1.1[dat.s1.1[, S] == 1, Y]), seed = seed)
       xi_model_hurt <- probability_forest(as.matrix(dat.s1.0[dat.s1.0[, S] == 1, X]), as.factor(dat.s1.0[dat.s1.0[, S] == 1, Y]), seed = seed)
     }
-    
+
     xi_pred_help <- predict(xi_model_help, newdata = (as.matrix(X_eval_k)))
     xi_pred_hurt <- predict(xi_model_hurt, newdata = (as.matrix(X_eval_k)))
-    
+
     xi_help <- xi_pred_help$predictions[, 1]
     xi_hurt <- xi_pred_hurt$predictions[, 1]
-    
+
     Y_eval_k[is.na(Y_eval_k)] <- 0
-    
+
     score1s_l_help <- (one_q_l - xi_help) * as.numeric(one_q_l >= xi_help) * one_q1
     score1s_u_help <- (one_q_l + (1 - xi_help - one_q_l) * as.numeric(one_q_l >= 1 - xi_help)) * one_q1
-    
+
     score0s_help <- (Y_eval_k * S_eval_k) * ((1 - D_eval_k) / (1 - Ps_eval_k))
-    
+
     score0s_l_hurt <- (one_q_l - xi_hurt) * as.numeric(one_q_l >= xi_hurt) * one_q0
     score0s_u_hurt <- (one_q_l + (1 - xi_hurt - one_q_l) * as.numeric(one_q_l >= 1 - xi_hurt)) * one_q0
-    
+
     score1s_hurt <- (Y_eval_k * S_eval_k) * (D_eval_k / Ps_eval_k)
-    
+
     qar_scores_help <- unlist(dat.s2[, S] * (1 - dat.s2[, D]) / (1 - Ps_eval_k))
-    
+
     qar_scores_hurt <- unlist(dat.s2[, S] * dat.s2[, D] / Ps_eval_k)
-    
+
     qar_total <- directions * qar_scores_help + (1 - directions) * qar_scores_hurt
-    
+
     scores_l_total <- directions * (score1s_l_help - score0s_help) + (1 - directions) * (score1s_hurt - score0s_u_hurt)
     scores_u_total <- directions * (score1s_u_help - score0s_help) + (1 - directions) * (score1s_hurt - score0s_l_hurt)
-    
+
     qar_k <- c(qar_k, qar_total)
-    
+
     scores_l_k <- c(scores_l_k, scores_l_total)
     scores_u_k <- c(scores_u_k, scores_u_total)
     cat(k, " ")
@@ -94,42 +94,42 @@ aggBounds_binary_oneshot <- function(dat_bo, seed = NULL, Y, X, S, D, W, Pscore,
   # qar_k <- directions_k * qar_help_k + (1 - directions_k) * qar_hurt_k
   # scores_l_k <- directions_k * (score1s_l_k - score0s_k) + (1 - directions_k) * (score1s_k - score0s_u_k)
   # scores_u_k <- directions_k * (score1s_u_k - score0s_k) + (1 - directions_k) * (score1s_k - score0s_l_k)
-  
+
   N <- sum(dat_bo[, W])
   tau_l_est_avg <- mean(scores_l_k) / mean(qar_k)
   tau_u_est_avg <- mean(scores_u_k) / mean(qar_k)
-  
+
   V_l <- matrix(NA, 2, 2)
   V_l[1, 1] <- var(scores_l_k)
   V_l[1, 2] <- V_l[2, 1] <- cov(scores_l_k, qar_k)
   V_l[2, 2] <- var(qar_k)
-  
+
   V_u <- matrix(NA, 2, 2)
   V_u[1, 1] <- var(scores_u_k)
   V_u[1, 2] <- V_u[2, 1] <- cov(scores_u_k, qar_k)
   V_u[2, 2] <- var(qar_k)
-  
+
   deriv_l <- c(1/mean(qar_k), -tau_l_est_avg/mean(qar_k))
   deriv_u <- c(1/mean(qar_k), -tau_u_est_avg/mean(qar_k))
-  
+
   se_tau_l_est_avg <- sqrt(t(deriv_l) %*% V_l %*% deriv_l / N)
   se_tau_u_est_avg <- sqrt(t(deriv_u) %*% V_u %*% deriv_u / N)
-  
+
   result <- c(tau_l_est_avg, tau_u_est_avg, se_tau_l_est_avg, se_tau_u_est_avg)
   return(result)
 }
 
 aggBounds_binary <- function(dat_final, seed = NULL, Y, X, S, D, W, Pscore,
-                             regression.splitting = F, cv_fold = cv_fold, trim_l, trim_u){
-  
+                             splitting = F, cv_fold = cv_fold, trim_l, trim_u){
+
   ests <- aggBounds_binary_oneshot(dat_bo = dat_final, seed = seed, Y, X, S, D, W, Pscore,
-                                   regression.splitting = F, cv_fold = cv_fold, trim_l, trim_u)
-  
+                                   splitting = F, cv_fold = cv_fold, trim_l, trim_u)
+
   tau_l_est_avg <- ests[1]
   tau_u_est_avg <- ests[2]
   se_tau_l_est_avg <- ests[3]
   se_tau_u_est_avg <- ests[4]
-  
+
   # ndat <- nrow(dat_final)
   # ests_boot <- matrix(NA, Nboots, 2)
   # for (b in 1:Nboots){
@@ -137,12 +137,12 @@ aggBounds_binary <- function(dat_final, seed = NULL, Y, X, S, D, W, Pscore,
   #   ests_boot[b, ] <- aggBounds_binary_oneshot(dat_boot, seed = NULL, Y, X, S, D, W, Pscore,
   #                                              regression.splitting = F, cv_fold = cv_fold, trim_l, trim_u)
   # }
-  # 
+  #
   # se_tau_l_est_avg <- sqrt(var(ests_boot[, 1]))
   # se_tau_u_est_avg <- sqrt(var(ests_boot[, 2]))
-  
+
   result <- list("tau_l_est_avg" = tau_l_est_avg,
-                 "tau_u_est_avg" = tau_u_est_avg, 
+                 "tau_u_est_avg" = tau_u_est_avg,
                  "se_tau_l_est_avg" = se_tau_l_est_avg,
                  "se_tau_u_est_avg" = se_tau_u_est_avg)
   return(result)
@@ -150,10 +150,10 @@ aggBounds_binary <- function(dat_final, seed = NULL, Y, X, S, D, W, Pscore,
 
 
 LBounds_binary_oneshot <- function(dat_bo, Y, X, S, D, W, direction){
-  
+
   q0_lee <- sum(dat_bo[, S] * (1-dat_bo[, D]) * dat_bo[, W]) / sum((1-dat_bo[, D]) * dat_bo[, W])
   q1_lee <- sum(dat_bo[, S] * dat_bo[, D] * dat_bo[, W]) / sum(dat_bo[, D] * dat_bo[, W])
-  
+
   if (direction == 1){
     q_lee <- q0_lee / q1_lee
     xi_lee <- sum(dat_bo[, S] * dat_bo[, D] * dat_bo[, W] * (1 - dat_bo[, Y]), na.rm = 1) / sum(dat_bo[, S] * dat_bo[, D] * dat_bo[, W])
@@ -177,7 +177,7 @@ LBounds_binary_oneshot <- function(dat_bo, Y, X, S, D, W, direction){
 
 LBounds_binary <- function(dat_final, Y, X, S, D, W, Pscore,
                            cond.mono = FALSE, direction = NULL, Nboots = 1000){
-  
+
   if (cond.mono == 1){
     if (is.null(seed)){
       pf0 <- probability_forest(X = as.matrix(dat_final[dat_final[, D] == 0, X]), Y = as.factor(unlist(dat_final[dat_final[, D] == 0, S])), sample.weights = dat_final[dat_final[, D] == 0, W])
@@ -186,27 +186,27 @@ LBounds_binary <- function(dat_final, Y, X, S, D, W, Pscore,
       pf0 <- probability_forest(X = as.matrix(dat_final[dat_final[, D] == 0, X]), Y = as.factor(unlist(dat_final[dat_final[, D] == 0, S])), sample.weights = dat_final[dat_final[, D] == 0, W], seed = seed)
       pf1 <- probability_forest(X = as.matrix(dat_final[dat_final[, D] == 1, X]), Y = as.factor(unlist(dat_final[dat_final[, D] == 1, S])), sample.weights = dat_final[dat_final[, D] == 1, W], seed = seed)
     }
-    
+
     pf0_pred <- predict(pf0, newdata = (as.matrix(dat_final[, X])))
     pf1_pred <- predict(pf1, newdata = (as.matrix(dat_final[, X])))
-    
+
     one_q0 <- pf0_pred$predictions[, 2]
     one_q1 <- pf1_pred$predictions[, 2]
-    
+
     directions <- as.numeric(one_q0 < one_q1)
-    
+
     dat_help <- dat_final[directions == 1, ]
     dat_hurt <- dat_final[directions == 0, ]
-    
+
     ndat_help <- nrow(dat_help)
     ndat_hurt <- nrow(dat_hurt)
-    
+
     # hurt lee bounds
     ests_lee_hurt <- LBounds_binary_oneshot(dat_hurt, Y, X, S, D, W, direction = 0)
-    
+
     tau_u_lee_hurt <- ests_lee_hurt[2]
     tau_l_lee_hurt <- ests_lee_hurt[1]
-    
+
     ests_lee_boot_hurt <- matrix(NA, Nboots, 2)
     for (b in 1:Nboots){
       dat_hurt_boot <- dat_hurt[sample(ndat_hurt, ndat_hurt, replace = 1), ]
@@ -215,16 +215,16 @@ LBounds_binary <- function(dat_final, Y, X, S, D, W, Pscore,
         cat(b, "\n")
       }
     }
-    
+
     se_tau_l_lee_hurt <- sqrt(var(ests_lee_boot_hurt[, 1]))
     se_tau_u_lee_hurt <- sqrt(var(ests_lee_boot_hurt[, 2]))
-    
+
     # help lee bounds
     ests_lee_help <- LBounds_binary_oneshot(dat_help, Y, X, S, D, W, direction = 1)
-    
+
     tau_u_lee_help <- ests_lee_help[2]
     tau_l_lee_help <- ests_lee_help[1]
-    
+
     ests_lee_boot_help <- matrix(NA, Nboots, 2)
     for (b in 1:Nboots){
       dat_help_boot <- dat_help[sample(ndat_help, ndat_help, replace = 1), ]
@@ -233,62 +233,62 @@ LBounds_binary <- function(dat_final, Y, X, S, D, W, Pscore,
         cat(b, "\n")
       }
     }
-    
+
     se_tau_l_lee_help <- sqrt(var(ests_lee_boot_help[, 1]))
     se_tau_u_lee_help <- sqrt(var(ests_lee_boot_help[, 2]))
-    
+
     pos_ratio <- mean(directions)
-    
+
     tau_u_lee <- pos_ratio * tau_u_lee_help + (1-pos_ratio) * tau_u_lee_hurt
     tau_l_lee <- pos_ratio * tau_l_lee_help + (1-pos_ratio) * tau_l_lee_hurt
-    
+
     se_tau_l_lee <- sqrt(pos_ratio^2 * se_tau_l_lee_help^2 + (1-pos_ratio)^2 * se_tau_l_lee_hurt^2)
     se_tau_u_lee <- sqrt(pos_ratio^2 * se_tau_u_lee_help^2 + (1-pos_ratio)^2 * se_tau_u_lee_hurt^2)
-    
+
   }else{
     ndat <- nrow(dat_final)
     q0_lee <- sum(dat_final[, S] * (1-dat_final[, D]) * dat_final[, W]) / sum((1-dat_final[, D]) * dat_final[, W])
     q1_lee <- sum(dat_final[, S] * dat_final[, D] * dat_final[, W]) / sum(dat_final[, D] * dat_final[, W])
-    
+
     if (is.null(direction)){
       direction <- as.numeric(q0_lee < q1_lee)
     }
     if (direction == 1){
       cat("S(1) > S(0)", "\n")
-      
+
       ests_lee <- LBounds_binary_oneshot(dat_final, Y, X, S, D, W, direction = 1)
-      
+
       tau_u_lee <- ests_lee[2]
       tau_l_lee <- ests_lee[1]
-      
+
       ests_lee_boot <- matrix(NA, Nboots, 2)
       for (b in 1:Nboots){
         dat_final_boot <- dat_final[sample(ndat, ndat, replace = 1), ]
         ests_lee_boot[b, ] <- LBounds_binary_oneshot(dat_final_boot, Y, X, S, D, W, direction = 1)
       }
-      
+
       se_tau_l_lee <- sqrt(var(ests_lee_boot[, 1]))
       se_tau_u_lee <- sqrt(var(ests_lee_boot[, 2]))
     }else if (direction == 0){
       cat("S(0) > S(1)", "\n")
       ests_lee <- LBounds_binary_oneshot(dat_final, Y, X, S, D, W, direction = 0)
-      
+
       tau_u_lee <- ests_lee[2]
       tau_l_lee <- ests_lee[1]
-      
+
       ests_lee_boot <- matrix(NA, Nboots, 2)
       for (b in 1:Nboots){
         dat_final_boot <- dat_final[sample(ndat, ndat, replace = 1), ]
         ests_lee_boot[b, ] <- LBounds_binary_oneshot(dat_final_boot, Y, X, S, D, W, direction = 0)
       }
-      
+
       se_tau_l_lee <- sqrt(var(ests_lee_boot[, 1]))
       se_tau_u_lee <- sqrt(var(ests_lee_boot[, 2]))
     }else{
       stop("Direction can only be 0 or 1")
     }
   }
-  result <- list("tau_l_est_lee" = tau_l_lee, 
+  result <- list("tau_l_est_lee" = tau_l_lee,
                  "tau_u_est_lee" = tau_u_lee,
                  "se_tau_l_est_lee" = se_tau_l_lee,
                  "se_tau_u_est_lee" = se_tau_u_lee)
